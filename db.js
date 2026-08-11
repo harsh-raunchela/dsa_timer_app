ca7df5f203a6d186c48c6b45be37d184f17c2676
harsh-raunchela <raunchelaharsh@gmail.com>
2026-08-11 06:21:07 +0530

    Configure Aiven MySQL port

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ const mysql = require("mysql2");
 
 const dbConfig = {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
@@ -19,14 +20,12 @@ if (process.env.NODE_ENV === "production") {
 const db = mysql.createConnection(dbConfig);
 
 db.connect((err) => {
-
     if (err) {
         console.error("Database connection failed:", err);
         return;
     }
 
     console.log("Connected to MySQL");
-
 });
 
 module.exports = db;
